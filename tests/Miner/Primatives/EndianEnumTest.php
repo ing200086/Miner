781438b1999135b5e4bd1ec976169a5c3140f4f9
= <=>
2017-12-04 18:11:59 -0600

    Increasing coverage for all classes

---
+++ tests/Miner/Primatives/EndianEnumTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * miner.
+ *
+ * @license MIT
+ * @author Ing200086 <ing200086@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Ing200086\Miner\Tests\Primatives;
+
+use Ing200086\Miner\Enums\Endian;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group  Focus
+ */
+class EndianEnumTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function canCreateValidEnum()
+    {
+        $enum = new Endian(Endian::BIG);
+
+        $this->assertInstanceOf(Endian::class, $enum);
+    }
+
+    /**
+     * @test
+     */
+    public function canThrowErrorForInvalidEnum()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $enum = new Endian('NotReal');
+    }
+
+    /**
+     * @test
+     */
+    public function endianObjectWillHaveStringValue()
+    {
+        $enum = new Endian(Endian::BIG);
+
+        $this->assertEquals(Endian::BIG, $enum);
+    }
+}
